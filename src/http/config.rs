47e9aa757a1cf1640fb097a6bf26d7dd298766d2
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-05-06 11:17:29 +0700

    Add database URL for config

--- src/http/config.rs
+++ src/http/config.rs
@@ -6,8 +6,13 @@ pub struct Config {
     /// The port to listen on.
     /// Set by the PORT environment variable.
     /// If not set, defaults to 3000.
-    #[arg(env = "PORT", default_value_t = 3000, required = true)]
-    port: u16,
+    #[arg(env = "PORT", default_value_t = 3000)]
+    pub(super) port: u16,
+
+    /// URL of MySQL database.
+    /// Set by the DATABASE_URL environment variable.
+    #[arg(env = "DATABASE_URL", required = true)]
+    pub(super) database_url: String,
 }
 
 #[tracing::instrument]
