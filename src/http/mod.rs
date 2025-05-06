843cfb8bf08d222dc455b7c1d580583f4879fd01
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-05-06 10:47:36 +0700

    Add serve boilerplate code

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,1 +1,9 @@
 pub mod config;
+
+use anyhow::Result;
+use config::Config;
+use tokio::net::TcpListener;
+
+pub async fn serve(config: Config, listener: TcpListener) -> Result<()> {
+    unimplemented!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+#![forbid(unsafe_code)]
+#![warn(clippy::correctness, clippy::pedantic, clippy::style, clippy::perf)]
+
+mod http;
+
+use anyhow::{Context, Result};
+use tokio::net::TcpListener;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize tracing subscriber
+    tracing_subscriber::fmt::init();
+
+    // Load application configuration
+    let config = http::config::load()?;
+
+    // Set up listening socket
+    // TODO: Read port from environment variable PORT
+    let port = 3000;
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = TcpListener::bind(&addr)
+        .await
+        .with_context(|| format!("Failed to bind to address {}", addr))?;
+    info!("Listening on port {}", port);
+
+    // Delegate startup to server
+    http::serve(config, listener)
+        .await
+        .with_context(|| format!("Failed to start server on {}", addr))
 }
