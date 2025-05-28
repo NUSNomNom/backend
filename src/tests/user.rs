21dda32817133e36d96beba0998d5c3f28a7d373
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-05-28 13:59:21 +0700

    Add test placeholder

--- src/tests/user.rs
+++ src/tests/user.rs
@@ -1,7 +1,16 @@
-#[tokio::test]
-async fn test_user_create() {
-}
+use anyhow::Result;
 
-#[tokio::test]
-async fn test_user_fetch() {
+use super::make_test_app;
+
+mod create {
+    use super::*;
+
+    #[tokio::test]
+    async fn new_user() -> Result<()> {
+        let app = make_test_app()
+            .await
+            .expect("Failed to create test app");
+        
+        Ok(())
+    }
 }
