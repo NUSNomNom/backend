07438f254dde7706d46f82662ff1a611308f4564
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-05-29 12:18:57 +0700

    Remove `v1` API path

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,3 +1,5 @@
+mod user;
+
 use axum::{
     Router,
     routing::{get, post, put},
@@ -5,15 +7,13 @@ use axum::{
 
 use crate::state::AppState;
 
-mod v1;
-
 pub(crate) fn make_router<S: AppState>() -> Router<S> {
     Router::new()
-        .route("/api/v1/user", post(v1::user::create::<S>))
-        .route("/api/v1/user", get(v1::user::fetch::<S>))
-        .route("/api/v1/user/password", put(v1::user::update_password::<S>))
+        .route("/api/user", post(user::create::<S>))
+        .route("/api/user", get(user::fetch::<S>))
+        .route("/api/user/password", put(user::update_password::<S>))
         .route(
-            "/api/v1/user/recovery",
-            post(v1::user::reset_password_request::<S>),
+            "/api/user/recovery",
+            post(user::reset_password_request::<S>),
         )
 }
