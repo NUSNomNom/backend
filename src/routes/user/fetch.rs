0d2094dd36f9c3e5a297f2d9c1cd5f95d3051b82
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-06-09 15:00:03 +0700

    Implement handler for fetching user

--- src/routes/user/fetch.rs
+++ src/routes/user/fetch.rs
@@ -1,5 +1,20 @@
-use axum::{extract::State, response::IntoResponse};
+use axum::{Json, extract::State, response::IntoResponse};
+use serde::Serialize;
 
-use crate::state::AppState;
+use crate::{models::Nomer, state::AppState};
 
-pub(super) async fn handle(State(_): State<AppState>) -> impl IntoResponse {}
+pub(super) async fn handle(State(_): State<AppState>, nomer: Nomer) -> impl IntoResponse {
+    Json(FetchResponse {
+        id: nomer.id,
+        display_name: nomer.display_name,
+        email: nomer.email,
+    })
+}
+
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct FetchResponse {
+    pub id: i64,
+    pub display_name: String,
+    pub email: String,
+}
