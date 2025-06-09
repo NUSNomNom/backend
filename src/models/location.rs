4a9378f8a186c40c03b507e369aade58a995b2a4
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-06-09 22:16:47 +0700

    Use `BigDecimal` to store coordinate

--- src/models/location.rs
+++ src/models/location.rs
@@ -1,9 +1,10 @@
+use bigdecimal::BigDecimal;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Location {
     pub id: i64,
     pub name: String,
-    pub longitude: f64,
-    pub latitude: f64,
+    pub longitude: BigDecimal,
+    pub latitude: BigDecimal,
 }
