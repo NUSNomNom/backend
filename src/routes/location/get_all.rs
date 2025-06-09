34fd36bbe9414dc81ffecca9fda752d7986a5972
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-06-09 21:48:35 +0700

    Implement handler for `GET /location`

--- src/routes/location/get_all.rs
+++ src/routes/location/get_all.rs
@@ -1,9 +1,25 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse};
+use axum::{Json, extract::State, http::StatusCode, response::IntoResponse};
+use tracing::error;
 
-use crate::state::AppState;
+use crate::{models::Location, state::AppState};
 
-pub(super) async fn handle(
-    State(state): State<AppState>,
-) -> impl IntoResponse {
-    (StatusCode::INTERNAL_SERVER_ERROR, "Not implemented")
+pub(super) async fn handle(State(state): State<AppState>) -> impl IntoResponse {
+    match sqlx::query_as!(
+        Location,
+        r#"SELECT
+            Id as id,
+            Name as name,
+            Longitude as longitude,
+            Latitude as latitude
+        FROM Location"#
+    )
+    .fetch_all(state.db())
+    .await
+    {
+        Ok(locations) => (StatusCode::OK, Json(locations)).into_response(),
+        Err(err) => {
+            error!("Failed to fetch locations: {}", err);
+            (StatusCode::INTERNAL_SERVER_ERROR, "Database error").into_response()
+        }
+    }
 }
