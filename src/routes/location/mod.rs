e5073fc55e07de3c348c969483f29bd6c5173b55
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-06-10 09:37:35 +0700

    Fix wrong method types

--- src/routes/location/mod.rs
+++ src/routes/location/mod.rs
@@ -1,14 +1,11 @@
 mod get_all;
 mod get_one;
-use axum::{
-    Router,
-    routing::{post, put},
-};
+use axum::{Router, routing::get};
 
 use crate::state::AppState;
 
 pub(super) fn make_router() -> Router<AppState> {
     Router::new()
-        .route("/", post(get_all::handle))
-        .route("/{id}", put(get_one::handle))
+        .route("/", get(get_all::handle))
+        .route("/{id}", get(get_one::handle))
 }
