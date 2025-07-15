ada9724ee61b9570653fe4ad61f8fcb84e8d65c5
Nguyen Thai Binh <binhnt.dev@icloud.com>
2025-07-15 15:48:51 +0700

    Add database model

--- src/routes/review/mod.rs
+++ src/routes/review/mod.rs
@@ -1,8 +1,13 @@
-mod remove;
-mod read_one;
-mod read_many;
 mod create;
-use axum::{routing::{delete, get, post}, Router};
+mod read_many;
+mod read_one;
+mod remove;
+use axum::{
+    Router,
+    routing::{delete, get, post},
+};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
 use crate::state::AppState;
 
@@ -12,4 +17,14 @@ pub(super) fn make_router() -> Router<AppState> {
         .route("/", get(read_many::handle))
         .route("/:id", get(read_one::handle))
         .route("/:id", delete(remove::handle))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DbReview {
+    review_id: i64,
+    score: i64,
+    comment: String,
+    nomer_id: i64,
+    store_id: i64,
+    created_at: DateTime<Utc>,
 }
